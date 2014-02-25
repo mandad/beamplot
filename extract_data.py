dc3f0f05320fe4e1de9d2ab99230c90a7598de01
Damian Manda <damian9@gmail.com>
2014-02-25 15:40:25 -0500

    Final version 2013

--- extract_data.py
+++ extract_data.py
@@ -5,10 +5,16 @@ def line_to_list(linetxt):
     return [float(x) for x in linetxt.split(" ")]
 
 # Open and read the data file
-datafile = open(sys.argv[1])
-ranges = line_to_list(datafile.readline())
-b_takeoff = line_to_list(datafile.readline())
-b_dir = line_to_list(datafile.readline())
+def process_RMB(datafile):
+    ranges = line_to_list(datafile.readline())
+    b_takeoff = line_to_list(datafile.readline())
+    b_dir = line_to_list(datafile.readline())
 
-# Save out to a numpy file
-np.savez(sys.argv[2], ranges=ranges, b_takeoff=b_takeoff, b_dir=b_dir)
+def main():
+    # Save out to a numpy file
+    datafile = open(sys.argv[1])
+    process_RMB(datafile)
+    np.savez(sys.argv[2], ranges=ranges, b_takeoff=b_takeoff, b_dir=b_dir)
+
+if __name__ == '__main__':
+    main()
